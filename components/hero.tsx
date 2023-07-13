e7c90d4de3d717f2987fe61d979f643121c5c7b7
KimGulmatico <gulmaticokim@protonmail.com>
2023-07-13 19:40:09 +1000

    fx3

--- components/hero.tsx
+++ components/hero.tsx
@@ -101,7 +101,7 @@ const Hero = () => {
               src="/ASCII.png"
               alt="Picture of the author"
               width="500"
-              height="1000"
+              height="533.79"
             />
           </div>
         </div>
