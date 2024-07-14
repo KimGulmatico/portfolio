da0fde649ca323bb9a2cb6fd8965128b13979187
kim-fm <127915878+kim-fm@users.noreply.github.com>
2024-07-14 12:29:47 +1000

    add insights and update package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kim web dev',
-  description: 'Portfolio of Kim G',
-}
+  title: "Kim web dev",
+  description: "Portfolio of Kim G",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-grid-white/[0.05]`}>{children}</body>
+      <body className={`${inter.className} bg-grid-white/[0.05]`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
