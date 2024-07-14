bfd6065191854fe2c570dcc5816a392b78df46ed
kim-fm <127915878+kim-fm@users.noreply.github.com>
2024-07-14 12:34:16 +1000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-grid-white/[0.05]`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
