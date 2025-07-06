78bdc700cd86d5299b28b48efcea4e5c3e289755
KimGulmatico <gulmaticokim@protonmail.com>
2025-07-06 22:22:03 +1000

    fix links

--- app/context/page.tsx
+++ app/context/page.tsx
@@ -49,13 +49,6 @@ export default async function Page() {
       </head>
       <body>
         <header>
-          <nav>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </nav>
           <h1>Welcome to My Portfolio</h1>
         </header>
         <main id="main-content">
